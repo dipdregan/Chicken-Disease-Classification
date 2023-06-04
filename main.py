afbfcece1789c624e642ebb6a3989650114bc8ac
Dipendra Pratap <dipendrapratap155@gmail.comgit config --global user.email dipendrapratap155@gmail.com>
2023-06-04 13:23:19 -0700

    i completed data ingestion part

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to my custom log")
+STAGE_NAME = 'Data Ingestion Stage'
+
+if __name__=="__main__":
+    try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+    except Exception as e:
+        logger.exception(e)
+        raise e
